0aa07a37d01dc1f06ee81bb7043a1de290650473
Patricio Asmet <patricioasmet@gmail.com>
2023-10-30 17:21:13 -0300

    ejercicio nro1 resuelto

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,3 +1,10 @@
 /* 1- Escribir un programa que solicite la edad y si es mayor de 18 años
       mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
  */
+const edad = parseInt(prompt('Ingrese la edad de la persona'));
+
+if(edad >= 18) {
+      document.write("Tiene edad permitida para conducir");
+}else{
+      document.write("Aun no tiene edad para conducir");
+}
